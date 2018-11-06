704a23f061d4acf6a0cc6aebbd76b3a8103fb226
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-06 13:46:04 -0600

    homestead: new server directory

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const Photos = require('../database/database.js');
 
 const app = express();
-const port = process.env.PORT || 1337;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
