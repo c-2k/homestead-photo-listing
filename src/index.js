31706bdaa5d9a03dc51a668dc3cb9c0b099c62e1
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-08 00:17:30 -0600

    homesteadPhoto: got deployed on elastic beanstalk

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
 
 
-// ReactDOM.render(<App />, document.getElementById('photoListing'));
+ReactDOM.render(<App />, document.getElementById('photoListing'));
 window.Gallery = App;
