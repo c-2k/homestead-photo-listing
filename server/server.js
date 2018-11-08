6a694b00725dcd8f5456a489e79062e00b8f5dc4
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-08 17:24:41 -0600

    photoListing: fixed seeding
    got seeding script to work

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const Photos = require('../database/database.js');
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 8081;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
