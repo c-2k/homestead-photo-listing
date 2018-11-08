a6bb365ee678a9c1ebd8a26c74869e1646a5b979
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-08 16:31:01 -0600

    photoListing: remove PORT from server
    got rid of port from server and hopefully fixed it

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const Photos = require('../database/database.js');
 
 const app = express();
-const port = process.env.PORT || 8081;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
