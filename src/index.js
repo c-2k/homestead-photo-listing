fa8213bccda2cdbfda9a4b472ebd2fc9145e4da5
Jackie Chen <chen.jackie.k@gmail.com>
2018-11-08 22:28:41 -0600

    homesteadPhoto: changed gallery name

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './components/app.jsx';
 
 
-ReactDOM.render(<App />, document.getElementById('photoListing'));
+ReactDOM.render(<App />, document.getElementById('Gallery'));
 window.Gallery = App;
